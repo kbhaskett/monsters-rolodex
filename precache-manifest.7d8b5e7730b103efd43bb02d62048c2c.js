630397b81094eec7fd6ebd9e13269c264f3ca2ec
Kevin Haskett <kevin.haskett@siriuscom.com>
2020-01-16 15:23:41 -0600

    Updates

---
+++ precache-manifest.7d8b5e7730b103efd43bb02d62048c2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0069e07c56380f4e46345c5187158c0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5ba3fe843726e33d3b88",
+    "url": "/monsters-rolodex/static/css/main.e09ba453.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ba3fe843726e33d3b88",
+    "url": "/monsters-rolodex/static/js/main.4b34006f.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
